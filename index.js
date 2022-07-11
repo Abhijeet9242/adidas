58763de243b6e3c71dc04793197697faf13e68ba
Abhijeet Kumar <105278913+abhijeet7781@users.noreply.github.com>
2022-07-11 23:25:22 +0530

    clone website

---
+++ index.js
@@ -0,0 +1,11 @@
+function showMen(){
+    window.location.href = "men_product.html";
+}
+
+function showWomen(){
+    window.location.href = "women_product.html";
+}
+
+function showKids(){
+    window.location.href = "kids_product.html";
+}
